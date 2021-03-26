40f103a78494ce75326b52f94169e8c152e58dfb
Parjanya Vyas <vyas.parjanya@gmail.com>
2021-03-26 14:17:51 -0400

    Adding logging for the hooks

--- app/src/main/java/com/parjanya/app2secapp/wrapper/base/hook/FrameworkHook.kt
+++ app/src/main/java/com/parjanya/app2secapp/wrapper/base/hook/FrameworkHook.kt
@@ -1,22 +1,27 @@
 package com.parjanya.app2secapp.wrapper.base.hook
 
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 
 class FrameworkHook(private val className: String,
                     private val hooks: List<HookMethodInfo>) {
 
     private fun hookMethod(hook: HookMethodInfo, classLoader: ClassLoader) {
+        XposedBridge.log("Initializing before hook for $className::${hook.methodName}")
         XposedHelpers.findAndHookMethod(className, classLoader, hook.methodName, object : XC_MethodHook() {
             override fun beforeHookedMethod(param: MethodHookParam?) {
+                XposedBridge.log("Intercepting hooked method $className::${hook.methodName}")
                 hook.beforeHook(param)
             }
         })
     }
 
     private fun hookConstructor(hook: HookMethodInfo, classLoader: ClassLoader) {
+        XposedBridge.log("Initializing before hook for $className::constructor")
         XposedHelpers.findAndHookConstructor(className, classLoader, object : XC_MethodHook() {
             override fun beforeHookedMethod(param: MethodHookParam?) {
+                XposedBridge.log("Intercepting hooked constructor for $className")
                 hook.beforeHook(param)
             }
         })
